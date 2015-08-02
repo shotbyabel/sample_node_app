c751f7abeab31702c66181147715c2df5e2b9dde
kimo5532 <shotbyabel@gmail.com>
2015-08-02 12:59:28 -0700

    create routes for the admin section

--- server.js
+++ server.js
@@ -1,13 +1,30 @@
 // load the express package and create our app
-var express = require('express');
-var app     = express();
-var path    = require('path');
-
+var express = require('express'),
+    app     = express(),
+    path    = require('path');
 // send our index.html file to the user for the home page
 app.get('/',function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
-
 });
+//CREATE ROUTES FOR THE ADMIN SECTION?//   
+//*1. get an instance of the router
+var adminRouter = express.Router();
+
+//*2. admin main page. the dashboard (http://localhost:1337/admin)
+adminRouter.get('/',function(req, res) {
+        res.send('I am the dashboard');
+});
+//*3. users page (http://localhost:1337/admin/users)
+adminRouter.get('/users', function(req, res) {
+        res.send('I Show all the users!');
+});
+//*4 posts page (http://localhost:1337/admin/posts)
+adminRouter.get('/posts', function(req, res) {
+        res.send('I show all the post');
+});
+//*5 apply the routes to our application
+app.use('/admin', adminRouter);
+
 // start the server
 app.listen(1337);
 console.log('1337 is the magic port!');
